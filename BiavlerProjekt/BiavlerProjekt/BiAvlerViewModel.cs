3cba918b2b0200b8583f52b437f21a1a9bbe0bb2
Emma Rolsted <emma-k-r@hotmail.com>
2018-12-28 00:22:23 +0100

    ...

--- BiavlerProjekt/BiavlerProjekt/BiAvlerViewModel.cs
+++ BiavlerProjekt/BiavlerProjekt/BiAvlerViewModel.cs
@@ -27,8 +27,9 @@ namespace BiavlerProjekt
             {
                 _fileName = value;
                 NotifyOfPropertyChange();
-            } 
+            }
         }
+
         public string Bistade1
         {
             get => _bistade;
@@ -73,13 +74,14 @@ namespace BiavlerProjekt
         {
             if (Bistade1 != null && Date1 != null && Count1 != null && Text1 != null)
             {
-                Collection.Add(new BiAvler() { Bistade = Bistade1, Date = Date1, Count = Count1, Text = Text1 });
+                Collection.Add(new BiAvler() {Bistade = Bistade1, Date = Date1, Count = Count1, Text = Text1});
                 Bistade1 = null;
                 Date1 = null;
                 Count1 = null;
                 Text1 = null;
             }
         }
+
         public ObservableCollection<BiAvler> Collection
         {
             get => _collection;
@@ -94,8 +96,10 @@ namespace BiavlerProjekt
         {
             if (filename != "" && Collection.Count > 0)
             {
-                XmlSerializer serializer = new XmlSerializer(typeof(BiAvler));
-                TextWriter writer = new StreamWriter(filename);
+
+
+                XmlSerializer serializer = new XmlSerializer(typeof(ObservableCollection<BiAvler>));
+                TextWriter writer = new StreamWriter(FileName);
                 // Serialize all the agents.
                 serializer.Serialize(writer, Collection);
                 writer.Close();
@@ -107,66 +111,109 @@ namespace BiavlerProjekt
             if (FileName != "")
             {
                 filename = FileName;
-                
+                Save();
+
             }
             else
                 MessageBox.Show("You must enter a file name in the File Name textbox!", "Unable to save file",
                     MessageBoxButton.OK, MessageBoxImage.Exclamation);
         }
 
-        //public void Close()
-        //{
-        //    Close();
-        //}
-
-    }
-
-}
-
-public class BiAvler : PropertyChangedBase
-{
-    private string _text;
-    private string _bistade;
-    private string _date;
-    private string _count;
-
-    public string Bistade
-    {
-        get => _bistade;
-        set
+        public void Open()
         {
-            _bistade = value;
-            NotifyOfPropertyChange();
+            filename = FileName;
+            ObservableCollection<BiAvler> tempAgents = new ObservableCollection<BiAvler>();
+
+            // Create an instance of the XmlSerializer class and specify the type of object to serialize.
+            XmlSerializer serializer = new XmlSerializer(typeof(ObservableCollection<BiAvler>));
+            try
+            {
+                TextReader reader = new StreamReader(filename);
+                // Deserialize all the agents.
+                tempAgents = (ObservableCollection<BiAvler>) serializer.Deserialize(reader);
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Unable to open file", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            // We have to insert the agents in the existing collection. If we just assign tempAgents to agents then the bindings to agents will brake!
+            Collection.Clear();
+            foreach (var agent in tempAgents)
+                Collection.Add(agent);
         }
-    }
 
-    public string Date
-    {
-        get => _date;
-        set
+        public void Close()
         {
-            _date = value;
-            NotifyOfPropertyChange();
+            MessageBoxResult result =
+                MessageBox.Show("Do you want to exit the application?", "Warning", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+
         }
-    }
 
-    public string Count
-    {
-        get => _count;
-        set
+        public void New()
         {
-            _count = value;
-            NotifyOfPropertyChange();
+            MessageBoxResult res = MessageBox.Show(
+                "Any unsaved data will be lost. Are you sure you want to initiate a new file?", "Warning",
+                MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
+            if (res == MessageBoxResult.Yes)
+            {
+                Collection.Clear();
+                filename = "";
+            }
         }
+
     }
 
-    public string Text
+    public class BiAvler : PropertyChangedBase
     {
-        get => _text;
-        set
+        private string _text;
+        private string _bistade;
+        private string _date;
+        private string _count;
+
+        public string Bistade
         {
-            _text = value;
-            NotifyOfPropertyChange();
+            get => _bistade;
+            set
+            {
+                _bistade = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+        public string Date
+        {
+            get => _date;
+            set
+            {
+                _date = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+        public string Count
+        {
+            get => _count;
+            set
+            {
+                _count = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+        public string Text
+        {
+            get => _text;
+            set
+            {
+                _text = value;
+                NotifyOfPropertyChange();
+            }
         }
     }
 }
